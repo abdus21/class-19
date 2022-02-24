986ff24df7236b9939abc2969278cf66f4f1b5d3
samad <yy5841453@gmail.com>
2022-02-24 18:04:56 +0600

    file uploaded

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function modal_view(){
             }
         });
 }
-function modal_edit(){
+function modal_edit(id){
    //const samad_modal = document.querySelector('.samad-modal');
     const edit_deves = document.querySelector('#edit_deves');
     const data_show = document.querySelector('.data_show');
@@ -50,6 +50,27 @@ function modal_edit(){
                 edit_deves.style.display = 'flex';
             }
         });
+        let name = document.querySelector('#e_name')
+        let email = document.querySelector('#e_email')
+        let cell = document.querySelector('#e_cell')
+        let photo = document.querySelector('#e_photo')
+        let skills = document.querySelector('#e_skil-load')
+        let img_preview = document.querySelector('#img_preview')
+        let get_id = document.querySelector('#get_id')
+
+        axios.get(`http://localhost:5050/deves/${id}`).then(res =>{
+
+                  get_id.value = id;
+                  name.value = res.data.name;
+                  email.value = res.data.email;
+                  cell.value = res.data.Cell;
+                  photo.value = res.data.photo;
+                  skills.value = res.data.skillId;
+                  img_preview.setAttribute('src', res.data.photo)
+        });
+        
+                  
+
 }
 function modal_delete(){
    //const samad_modal = document.querySelector('.samad-modal');
@@ -100,8 +121,9 @@ deves_add_form.addEventListener('submit', function(e){
     let email = this.querySelector('#email')
     let photo = this.querySelector('#photo')
     let skill = this.querySelector('#skil-load')
+    let Cell = this.querySelector('#cell')
 
-    if(name.value == '' || email.value == '' || photo.value == '' || skill.value == ''){
+    if(name.value == '' || email.value == '' || photo.value == '' || skill.value == '' || Cell.value == ''){
         alert('All fields are required!')
     }else{
         axios.post('http://localhost:5050/deves', {
@@ -109,14 +131,23 @@ deves_add_form.addEventListener('submit', function(e){
             name : name.value,
             email : email.value,
             photo : photo.value,
-            skill : skill.value,
-        }).then()
+            skillId : skill.value,
+            Cell  : Cell.value
+        }).then(res =>{
+            add_new_deveoper()
+        });
+       
     }
       this.querySelector('#name').value = '';
       this.querySelector('#email').value = '';
       this.querySelector('#photo').value = '';
-      this.querySelector('#skil-load').value = '';
-})
+      //this.querySelector('#skil-load').value = '';
+      this.querySelector('#cell').value = '';
+});
+
+
+
+
 
 //add_new_deveoper
 const table_data = document.querySelector('#table_data');
@@ -134,7 +165,7 @@ const add_new_deveoper = () =>{
             <td>0193883888</td>
             <td><img src="${data.photo}" style="object-fit:cover; width:50px; height:50px" alt=""></td>
             <td><a  onclick="modal_view()" class="btn btn-info btn-sm" href="#"><i class="fa-solid fa-eye"></i></a></td>
-            <td><a  onclick="modal_edit()" class="btn btn-warning btn-sm" href="#"><i class="fa-solid fa-pen-to-square"></i></a></td>
+            <td><a  onclick="modal_edit(${data.id})" class="btn btn-warning btn-sm" href="#"><i class="fa-solid fa-pen-to-square"></i></a></td>
             <td><a  onclick="modal_delete()" class="btn btn-danger btn-sm" href="#"><i class="fa-solid fa-trash"></i></a></td>
         </tr>
             `
@@ -144,4 +175,37 @@ const add_new_deveoper = () =>{
     })
 }
 
-add_new_deveoper()
+add_new_deveoper();
+
+
+//deves_edit_form
+
+const deves_edit_form = document.querySelector('#deves_edit_form')
+
+deves_edit_form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+        let name = document.querySelector('#e_name')
+        let email = document.querySelector('#e_email')
+        let cell = document.querySelector('#e_cell')
+        let photo = document.querySelector('#e_photo')
+        let skills = document.querySelector('#e_skil-load')
+
+
+        axios.patch(`http://localhost:5050/deves/${get_id.value}`,{
+            id   : "",
+            name : name.value,
+            email : email.value,
+            photo : photo.value,
+            skillId : skills.value,
+            Cell  : cell.value
+        }).then(res =>{
+            add_new_deveoper()
+        });
+
+        this.querySelector('#name').value = '';
+        this.querySelector('#email').value = '';
+        this.querySelector('#photo').value = '';
+        //this.querySelector('#skil-load').value = '';
+        this.querySelector('#cell').value = '';
+})
